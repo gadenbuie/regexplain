836bb8dea108e09dbda6d068859349de38f9a117
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-03-22 13:23:06 -0400

    Remove empty test

--- tests/testthat/test-wrap_regex.R
+++ tests/testthat/test-wrap_regex.R
@@ -13,5 +13,4 @@ test_that("wrap_regex doesn't wrap non-capture groups", {
   expect_equal(wrap_regex("(?:a)(b)"), "(?:a)<span class=\"g01\">(b)</span>")
   expect_equal(wrap_regex("((?:a(b))c)"),
                "<span class=\"g01\">((?:a<span class=\"g02\">(b)</span>)c)</span>")
-  expect_equal()
 })
