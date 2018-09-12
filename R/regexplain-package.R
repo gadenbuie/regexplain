ccdeb680749b3d49776521fb454d04a54a45f2b6
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-09-11 21:48:29 -0400

    Document options in package help text

--- R/regexplain-package.R
+++ R/regexplain-package.R
@@ -1,3 +1,8 @@
+#' @section Options:
+#' - `regexplain.no.check.version` if `TRUE` or unset, the app will check for
+#'   updates on the first run of the session.
+#' - `regexplain.addin.max_lines` controls the number of lines of text imported
+#'   into the gadget, defaults to 100. Higher values will degrade performance.
 #' @importFrom dplyr "%>%" mutate filter group_by summarize select
 #' @importFrom utils getFromNamespace installed.packages packageVersion globalVariables
 #' @importFrom rlang .data
